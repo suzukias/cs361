d37d6a3a7f24a28f6770af287fba6f9c9050c7fa
suzukias <mditcaslsby20@gmail.com>
2023-10-30 18:56:28 +0900

    created login feature

--- UI.py
+++ UI.py
@@ -2,24 +2,20 @@ USER_LISTS = "users.txt"
 
 
 def welcome_message():
-    print("Welcome to your Expense Tracker! \nThis is your personal expense management service. You can easily track and manage your expenses.")
+    print("\nWelcome to your Expense Tracker! \nThis is your personal expense management service. You can easily track and manage your expenses.")
 
 
 def login_or_signup():
     while True:
-        selection = input("""
-            Please Register or Login by making a selection and pressing Enter.
-            0 - Exit
-            1 - Registration
-            2 - Login
-            3 - Help
-            Selection: """)
+        selection = input(
+            "\nPlease Register or Login by making a selection and pressing Enter.\n0 - Exit\n1 - Registration\n2 - Login\n3 - Help\nSelection: ")
         if selection == "0":
             exit()
         elif selection == "1":
             registration()
         elif selection == "2":
-            print("login")
+            login()
+            return
         elif selection == "3":
             print("help page")
         else:
@@ -44,12 +40,7 @@ def password_checker(password):
 
 
 def registration():
-    print("""
-        REGISTRATION PAGE
-        Please complete the following to register or make a selection and press Enter.
-        0 - Exit
-        1 - Back
-        """)
+    print("\nREGISTRATION PAGE\nPlease complete the following to register or make a selection and press Enter.\n0 - Exit\n1 - Back")
     username = input("Please enter a username: ")
     if username == "0":
         exit()
@@ -79,6 +70,50 @@ def registration():
                 else:
                     print(
                         "Password is invalid. It must be 6 characters long and contain at least 1 number.")
+
+
+def login():
+    print("\nLogin PAGE\nPlease complete the following to login or make a selection and press Enter.\n0 - Exit\n1 - Back to Top")
+    username = input("Please input your username to login: ")
+    if username == "0":
+        exit()
+    elif username == "1":
+        login_or_signup()
+    else:
+        registered_users = get_registered_users()
+        if username in registered_users:
+            password = input("Please enter your password: ")
+            password_matched = False
+            with open("users.txt", "r") as file:
+                for line in file:
+                    stored_username, stored_password = line.strip().split(',', 1)
+                    if username == stored_username and password == stored_password:
+                        print("Login successful!")
+                        password_matched = True
+                        top_page()
+                        break
+            if not password_matched:
+                print(
+                    "Invalid password. Your password is 6 characters long and contains at least 1 number.")
+                retry = input(
+                    "\nDo you want to try login again?\n0 - Exit\n1 - Back to Top\n2 - Try Again\nSelection: ")
+                if retry == "0":
+                    exit()
+                elif retry == "1":
+                    login_or_signup()
+                elif retry == "2":
+                    login()
+                else:
+                    print(
+                        "Invalid input. Please register or login again to use the service.")
+                    login_or_signup()
+        else:
+            print("Username not found. Please register or try again.")
+            login_or_signup()
+
+
+def top_page():
+    print("work in progress")
 
 
 def main():
